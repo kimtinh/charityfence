a1dd2905cd690c78df7c2edd5342b2a78f6c3955
kimtinh <64942629+kimtinh@users.noreply.github.com>
2020-09-12 11:07:49 +0700

    fix

--- resources/views/view/home.blade.php
+++ resources/views/view/home.blade.php
@@ -92,8 +92,7 @@
                             <div class="campaign-main-info mb-auto w-100">
                                 <div class="campaign-badge-box d-flex justify-content-between align-items-center">
                                     <img width="50" height="50" class="mr-2 avatar bg-white img-fill"
-                                        src="{{asset($item->user->avatar ?? 'images/user.png')}}"
-                                        alt="">
+                                        src="{{asset($item->user->avatar ?? 'images/user.png')}}" alt="">
                                 </div>
                                 <a href="{{route('view.campaign.detail', $item->id)}}">
                                     <h6 class="card-title mt-3 mb-2 text-body ">{{$item->name}}</h6>
@@ -306,5 +305,17 @@
 @endsection
 
 @section('scripts')
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+    integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+</script>
+<script>
 $('.carousel').carousel()
+</script>
+
 @endsection
